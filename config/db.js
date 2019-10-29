9a7129e04458c03e22d1edd4d7d1946d896502c0
Silicob <Silicob>
2019-10-28 18:00:05 -0700

    change to connection string

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // edited to include my non-admin, user level account and PW on mongo atlas
 // and also to include the name of the mongo DB that the collection is in (mySaverDB)
 const dbURI =
-  "mongodb+srv://mySaverUser:40uJHycVRtkU3zwC@tangerine-birp8.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://mySaverUser:40uJHycVRtkU3zwC@tangerine-birp8.mongodb.net/mySaverDB?retryWrites=true&w=majority";
 
 // Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
 // by default, you need to set it to false.
