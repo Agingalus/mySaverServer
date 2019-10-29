a5e1db3f48e2173baa2be43ed5f3d28fc0eec90a
Aaron Greene <43617847+Agingalus@users.noreply.github.com>
2019-10-28 19:26:57 -0700

    i have no idea what i did

--- config/db.js
+++ config/db.js
@@ -4,13 +4,27 @@ const mongoose = require("mongoose");
 
 // edited to include my non-admin, user level account and PW on mongo atlas
 // and also to include the name of the mongo DB that the collection is in (mySaverDB)
-// !const dbURI =
-//!     "mongodb+srv://mySaverUser:40uJHycVRtkU3zwC@tangerine-birp8.mongodb.net/mySaverDB?retryWrites=true&w=majority";
 const dbURI =
-    "mongodb+srv://bcuser2:bcuser2@cluster0-nbt1n.mongodb.net/TaskDB?retryWrites=true&w=majority"
+    "mongodb+srv://mySaverUser:40uJHycVRtkU3zwC@tangerine-birp8.mongodb.net/mySaverDB?retryWrites=true&w=majority";
+// ?const dbURI =
+// ?    "mongodb+srv://bcuser2:bcuser2@cluster0-nbt1n.mongodb.net/TaskDB?retryWrites=true&w=majority";
+// const dbURI =
+//     "mongodb+srv://mySaverUser:40uJHycVRtkU3zwC@tangerine-birp8.mongodb.net/Transactions";
 
 // Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
 // by default, you need to set it to false.
+
+
+
+//var connectionString = "mongodb+srv://mySaverUser:40uJHycVRtkU3zwC@tangerine-birp8.mongodb.net/";
+// var mongoUrl = new MongoUrl(connectionString);
+// var dbname = mongoUrl.DatabaseName;
+// var db = new MongoClient(mongoUrl).GetDatabase(dbname);
+// console.log("this is my database")
+// console.log(db);
+
+
+
 mongoose.set('useFindAndModify', false);
 
 const options = {
@@ -18,7 +32,9 @@ const options = {
     poolSize: 10
 };
 
-mongoose.connect(dbURI, options).then(
+mongoose.connect(dbURI,
+    // { dbName: 'mySaverDB'}
+).then(
     () => {
         console.log("Database connection established!");
     },
