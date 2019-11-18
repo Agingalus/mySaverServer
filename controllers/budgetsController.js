64efc0400beda7fd7eb5e0d01445d7b13e4bc5e5
Aaron Greene <43617847+Agingalus@users.noreply.github.com>
2019-11-18 14:27:49 -0800

    got delete for buget to work

--- controllers/budgetsController.js
+++ controllers/budgetsController.js
@@ -40,68 +40,72 @@ exports.createNewBudget = (req, res) => {
     });
 };
 exports.deleteBudget = (req, res) => {
-    Budget.deleteOne({"_id":o_id}, (err, budget) => {  
-      console.log("inside DeleteOne the id is ", o_id);
-      
-      if (err) {
-        //res.status(404).send(err);
-        res.status(500).send(err);
-      }
-      res.status(200).json({ message: "Budget successfully deleted."});
+    var ObjectId = require('mongodb').ObjectId;
+    var id = req.params._id;
+    var o_id = new ObjectId(id);
+    let Budget = new Budgets(req.body);
+    Budget.deleteOne({ "_id": o_id }, (err, budget) => {
+        console.log("inside DeleteOne the id is ", o_id);
+
+        if (err) {
+            //res.status(404).send(err);
+            res.status(500).send(err);
+        }
+        res.status(200).json({ message: "Budget successfully deleted." });
     });
 };
 // exports.deleteTransaction = (req, res) => {
 
-  
+
 //     var ObjectId = require('mongodb').ObjectId;
 //     var id = req.params._id;       
 //     var o_id = new ObjectId(id);
-  
+
 //     console.log("o_id is ", o_id, " of type ", typeof(o_id));
 
 //     Budget.find(o_id, (err, budget) => {
-     
+
 //       console.log("the o_id is ", o_id);
-  
+
 //       if (err) {
 //         res.status(500).send(err);
 //       }
-  
+
 //       console.log(budget);
 //     });
-  
+
 //     //db.orders.deleteOne( { "_id" : ObjectId("563237a41a4d68582c2509da") } );
 //     //({student_staff_id:2})
-  
+
 //     //var uid = req.params._id;
 //     //{'_id': ObjectId(uid)}
-  
+
 //     //id2 = 'ObjectID("'+id+'")';
 //     //console.log(id2);
-  
+
 //     Budget.deleteOne({"_id":o_id}, (err, budget) => {  
 //       console.log("inside DeleteOne the id is ", o_id);
-      
+
 //       if (err) {
 //         //res.status(404).send(err);
 //         res.status(500).send(err);
 //       }
 //       res.status(200).json({ message: "Budget successfully deleted."});
 //     });
-  
+
 //     Budget.find(o_id, (err, budget) => {
 //       //console.log("the {_id:o_id} is ", {_id:o_id});
 //       console.log("the o_id is ", o_id);
-  
+
 //       if (err) {
 //         res.status(500).send(err);
 //       }
 //       //console.log("Response is", res);
-  
+
 //       //res.status(200).json(budget);
 //       console.log("Second find result");
 //       console.log(budget);
 //     });
-  
-  
+
+
 //   };
